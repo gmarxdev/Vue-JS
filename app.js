dc498cab1e8600901c2f52f998f8db4712116f61
gmarxdev <gmarbandoy@gmail.com>
2025-08-31 22:00:59 +0800

    u

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     // data, functions
     // template: "<h2>NIce VUe JS!</h2>"
-
+    data() {
+        return {
+            call: "call me ehe"
+        }
+    }
     
 })
 
