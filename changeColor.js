a1178c06c9b402e818b1e5cfa05d258a7bc55836
gmarxdev <gmarbandoy@gmail.com>
2025-09-01 21:50:12 +0800

    u

---
+++ changeColor.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+
+
+    data() {
+        return {
+            btnColor = 'blue'
+        }
+    },
+    methods: {
+        changeColor() {
+            this.btnColor = this.btnColor === 'blue' ? 'red' : 'blue';
+        }
+    }
+    
+})
+
+app.mount('#changeColor')
