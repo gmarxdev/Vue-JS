910c8943404b5f904d82685ede68c6759f34b95c
gmarxdev <gmarbandoy@gmail.com>
2025-09-03 04:21:56 +0800

    u

--- part2/app.js
+++ part2/app.js
@@ -2,11 +2,17 @@ const app = Vue.createApp({
 
     data() {
         return {
+            Book: [
+                { title: "The Habit Loop", author: "Charles Duhigg" }
+            ],
 
+            toggle: true,
         }
     },
     method: {
-
+        btnToggle() {
+            this.toggle = !this.toggle
+        }
     }
 
 })
