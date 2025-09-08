27e982188c294917fc82ed44a6a9edd5cf0fac9c
gmarxdev <gmarbandoy@gmail.com>
2025-09-08 13:01:52 +0800

    u

--- part2/app.js
+++ part2/app.js
@@ -5,9 +5,9 @@ const app = Vue.createApp({
             url: "https://github.com/gmarxdev",
             toggle: true,
             books: [
-                { title: 'The Habit Loop', author: 'Charles Duhigg', img: 'assets/my-photo.jpg' },
-                { title: 'The Habit Loop', author: 'Charles Duhigg', img: 'assets/my-photo.jpg' },
-                { title: 'The Habit Loop', author: 'Charles Duhigg', img: 'assets/my-photo.jpg' },
+                { title: 'The Habit Loop', author: 'Charles Duhigg', img: 'assets/my-photo.jpg', isFav: true },
+                { title: 'The Habit Loop', author: 'Charles Duhigg', img: 'assets/my-photo.jpg', isFav: false },
+                { title: 'NO EXCUSES!', author: 'Tracey', img: 'assets/my-photo.jpg', isFav: true },
             ]
 
         }
