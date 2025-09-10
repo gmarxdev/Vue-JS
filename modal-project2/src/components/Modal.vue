75b3c07aa4d7c9d94e5b957daa43ccd0ceda64dc
gmarxdev <gmarbandoy@gmail.com>
2025-09-10 14:56:17 +0800

    u

--- modal-project2/src/components/Modal.vue
+++ modal-project2/src/components/Modal.vue
@@ -15,12 +15,12 @@
         padding: 20px;
         margin: 100px auto;
         background: white;
-        border-radius: 20px;
+        border-radius: 10px;
     }
     .backdrop {
         top: 0;
         position: fixed;
-        background: rgba(0, 0, 0, 0.4);
+        background: rgba(0, 0, 0, 0.6);
         width: 100%;
         height: 100%;
     }
