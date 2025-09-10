e5a7c5d9bb136c74df72bde956128b8711f86860
gmarxdev <gmarbandoy@gmail.com>
2025-09-10 14:30:40 +0800

    u

--- modal-project2/src/App.vue
+++ modal-project2/src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ msg }}</h1>
 </template>
 
 <script>
@@ -9,7 +8,12 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      msg: "MODAL WILL POP UP HERE!"
+    }
   }
 }
 </script>
