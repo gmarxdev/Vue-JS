07241eff13e6f4c9ad1dafdb1eb4505b64468f4c
gmarxdev <gmarbandoy@gmail.com>
2025-09-14 22:40:10 +0800

    Overriding Custom CSS around the codebase

--- new-course-vue/src/main.js
+++ new-course-vue/src/main.js
@@ -1,9 +1,10 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 // Bootstrap Packages
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
+// Overriding Custom CSS around the codebase
+import './assets/main.css'
 
 createApp(App).mount('#app')
