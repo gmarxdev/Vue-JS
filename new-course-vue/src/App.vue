b569ce1429fc040560349a9702bee6bf2e746fb6
gmarxdev <gmarbandoy@gmail.com>
2025-09-14 23:33:13 +0800

    u

--- new-course-vue/src/App.vue
+++ new-course-vue/src/App.vue
@@ -3,14 +3,18 @@ import Header from './components/Header.vue'
 </script>
 
 <template>
-  <Header />
 
-  <main>
-    <!-- Your page content -->
-  </main>
+    <Header/>
+
+    <main>
+      
+    </main>
+
 </template>
 
 <style scoped>
+
 @media (min-width: 1024px) {
+
 }
 </style>
